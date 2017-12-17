80a71f8543234ad842f8dfe5148a75895ae1e9bf
Sergiy Sanin <sanin_sergiy@yahoo.com>
2017-12-17 16:19:15 +0800

    switching to bokeh

---
+++ bokeh_flask.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+import pandas as pd
+from datetime import datetime
+
+from bokeh.embed import server_document
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Slider
+from bokeh.plotting import figure
+from bokeh.server.server import Server
+from bokeh.themes import Theme
+
+app = Flask(__name__)
+
+
+def modify_doc(doc):
+    df = pd.read_csv('data/tsla.csv')[['Date', 'Adj Close']]
+    df.set_index('Date', inplace=True)
+    # source = ColumnDataSource(data=df)
+    plot = figure(x_axis_type='datetime', y_range=(15, 300), y_axis_label='Adj Close',
+                  title="Adjusted Close Price")
+    plot.line([datetime.strptime(d, '%Y-%m-%d') for d in df.index], list(df['Adj Close']))
+
+    doc.add_root(plot)
+    doc.theme = Theme(filename="theme.yaml")
+
+
+@app.route('/', methods=['GET'])
+def bkapp_page():
+    script = server_document('http://localhost:5006/bkapp')
+    return render_template("embed.html", script=script, template="Flask")
+
+
+def bk_worker():
+    # Can't pass num_procs > 1 in this configuration. If you need to run multiple
+    # processes, see e.g. flask_gunicorn_embed.py
+    server = Server({'/bkapp': modify_doc}, allow_websocket_origin=["localhost:8000"])
+    server.start()
+    server.io_loop.start()
+
+
+from threading import Thread
+Thread(target=bk_worker).start()
+
+if __name__ == '__main__':
+    print('Opening single process Flask app with embedded Bokeh application on http://localhost:8000/')
+app.run(port=8000, debug=True)
